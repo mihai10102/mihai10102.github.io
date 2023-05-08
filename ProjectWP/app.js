1befcfae68911f4bcfbf6b5d79bdb3ec77ad897b
mihai10102 <mihaibodnaruc70@gmail.com>
2023-05-08 08:46:09 +0300

    change this to xhttp

--- ProjectWP/app.js
+++ ProjectWP/app.js
@@ -19,8 +19,8 @@ function handleLogin() {
   var xhttp = new XMLHttpRequest();
 
   xhttp.onreadystatechange = function () {
-    if (this.readyState == 4 && this.status == 200) {
-      var data = JSON.parse(this.responseText);
+    if (xhttp.readyState == 4 && xhttp.status == 200) {
+      var data = JSON.parse(xhttp.responseText);
 
       var isLoggedIn = false;
 
